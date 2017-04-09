445ab2e1e90c745279609141d9a74a71e46ae91d
Sammers21 <titantins@ya.ru>
2017-04-09 12:25:06 +0300

    init GUI

---
+++ src/main/java/ru/drankov/client/ClientGUI.java
@@ -0,0 +1,11 @@
+package ru.drankov.client;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ClientGUI extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
---
+++ src/main/java/ru/drankov/server/ServerGUI.java
@@ -0,0 +1,11 @@
+package ru.drankov.server;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ServerGUI  extends Application{
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
